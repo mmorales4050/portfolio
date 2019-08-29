9bd2d7b157386385c7e240facf90d9965cdc3a25
mmorales4050 <mmorales4050@gmail.com>
2019-08-29 13:11:41 -0400

    add header image

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './Header.css'
+
+class Header extends Component {
+
+  render() {
+    return (
+      <div className="header">
+
+      </div>
+    );
+  }
+
+}
+
+export default Header;
